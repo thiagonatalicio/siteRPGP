3a48b436ae057ac0aeab1582dd51a4f7eb143687
thiagonatalicio <thiagonatalicio33@gmail.com>
2026-06-24 15:00:16 -0300

    menu inicial

---
+++ script.js
@@ -0,0 +1,12 @@
+const menu = document.getElementById("menu");
+const overlay = document.getElementById("overlay");
+
+function abrirMenu() {
+    menu.classList.add("active");
+    overlay.classList.add("active");
+}
+
+overlay.addEventListener("click", () => {
+    menu.classList.remove("active");
+    overlay.classList.remove("active");
+});
